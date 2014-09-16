15a28905a456e9a4370943752fe9cdbe1fb0f74f
Ben Sampson <ben@Bens-MacBook-Pro.local>
2014-09-16 18:23:16 -0400

    Label updates

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    
     @IBOutlet weak var messageLabel: UILabel!
 
     @IBOutlet weak var enterNameTextField: UITextField!
@@ -39,6 +41,12 @@ class ViewController: UIViewController {
         enterAMessageTextField.resignFirstResponder()
         messageLabel.textColor = UIColor.redColor()
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        enterNameTextField.text = ""
+  
     }
 
 }
